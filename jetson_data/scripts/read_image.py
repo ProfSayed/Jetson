e9321fa7837badc78bebf0037607525f4a3aab5d
Sayed ElSheikh <mec.sayed97@gmail.com>
2022-10-18 19:23:59 +0200

    go

--- jetson_data/scripts/read_image.py
+++ jetson_data/scripts/read_image.py
@@ -16,14 +16,15 @@ def main():
         img_pub = rospy.Publisher('/camera1/raw_image', Image,queue_size=3)
         rate = rospy.Rate(30)
 
-        
-        rospy.loginfo_once("Publishing Image")
-        try:
-            img_msg = bridge.cv2_to_imgmsg(cv_image,"bgr8")
-            img_pub.publish(img_msg)
-        except CvBridgeError as e:
-            rospy.logerr(e)
+        while not rospy.is_shutdown():
+            rospy.loginfo_once("Publishing Image")
+            try:
+                img_msg = bridge.cv2_to_imgmsg(cv_image,"bgr8")
+                img_pub.publish(img_msg)
+            except CvBridgeError as e:
+                rospy.logerr(e)
 
+            rate.sleep()
 
     except rospy.ROSInterruptException:
         pass
