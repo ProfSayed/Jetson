d6d3a04cb842fca17ec61d2db370ce7fc607e75a
Sayed ElSheikh <mec.sayed97@gmail.com>
2022-10-18 21:44:48 +0200

    go

--- jetson_data/scripts/read_image.py
+++ jetson_data/scripts/read_image.py
@@ -2,13 +2,13 @@
 import cv2
 import rospy
 import rospkg
-from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
 
 def main():
     rospy.init_node("Image_Publisher")
     rospack = rospkg.RosPack()
-    image_path = rospack.get_path('jetson_data') + '/samples/cyl.png'
+    image_path = rospack.get_path('jetson_detection') + '/samples/cyl.png'
     cv_image = cv2.imread(image_path)   
     cv_image = cv2.resize(cv_image,(256,256))
     try:
@@ -21,6 +21,7 @@ def main():
             try:
                 img_msg = bridge.cv2_to_imgmsg(cv_image,"bgr8")
                 img_pub.publish(img_msg)
+                rospy.loginfo(img_msg.data)
             except CvBridgeError as e:
                 rospy.logerr(e)
 
