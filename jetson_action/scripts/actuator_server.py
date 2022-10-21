d22ce136fea4c4271ba734312c2ce8a93935fdde
Sayed ElSheikh <mec.sayed97@gmail.com>
2022-10-21 12:28:07 +0200

    Finish

--- jetson_action/scripts/actuator_server.py
+++ jetson_action/scripts/actuator_server.py
@@ -4,7 +4,6 @@ import RPi.GPIO as GPIO
 from jetson_msgs.srv import Actuator,ActuatorResponse
 
 def actuate_cb(req):
-    rospy.sleep(delay_secs)
     GPIO.output(actuator_pin, req.actuate) 
     rospy.loginfo("%s Action: %s"%(rospy.get_name(), req.actuate))
     return ActuatorResponse(True)
@@ -13,7 +12,6 @@ if __name__ == "__main__":
     try:
         rospy.init_node('actuator_server')
         topic_name = rospy.get_param('~topic_name')
-        delay_secs = rospy.get_param('~delay_action')
         ## Initialize gpio
         actuator_pin = rospy.get_param('~gpio')
         GPIO.setmode(GPIO.BOARD)
