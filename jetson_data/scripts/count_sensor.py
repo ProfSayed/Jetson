ed03f7b5a3eb068f0a699b2dcc07655b1731e682
Sayed ElSheikh <mec.sayed97@gmail.com>
2022-10-28 15:37:25 +0200

    go

--- jetson_data/scripts/count_sensor.py
+++ jetson_data/scripts/count_sensor.py
@@ -36,10 +36,11 @@ def main():
             if new_cylinder:
                 new_cylinder = False
                 index += 1
-            ## Publish Message
-            msg.time_stamp = rospy.Time.now()
-            msg.cylinder_number = index
-            pub.publish(msg)
+                
+        ## Publish Message
+        msg.time_stamp = rospy.Time.now()
+        msg.cylinder_number = index
+        pub.publish(msg)
         
         ## Check for Overflow
         if ina219.overflow:
