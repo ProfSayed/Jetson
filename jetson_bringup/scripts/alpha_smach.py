b6512285e26f8ffe9d9c495a321f8c0b001829f9
Sayed ElSheikh <mec.sayed97@gmail.com>
2022-10-29 07:58:18 +0200

    go

--- jetson_bringup/scripts/alpha_smach.py
+++ jetson_bringup/scripts/alpha_smach.py
@@ -58,7 +58,7 @@ class If_cap(smach.State):
 
 def stopper_sensor_cb(ud, msg):
     rospy.loginfo('Cylinder %s Detected by Sensor'% msg.cylinder_number)
-    if ud.cylinder_number > msg.cylinder_number:
+    if ud.cylinder_number < msg.cylinder_number:
         ud.cylinder_number = msg.cylinder_number
         return False
     else:
